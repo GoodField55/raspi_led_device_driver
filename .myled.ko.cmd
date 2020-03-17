e0d66ff0e1f7fc31c8954047d6536d2686ab7c36
GoodField55 <goodfield055@gmail.com>
2020-03-17 07:46:27 +0000

    Delete unuse files

--- .myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/catkin_ws/src/raspi_led_device_driver/myled.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/ubuntu/catkin_ws/src/raspi_led_device_driver/myled.ko /home/ubuntu/catkin_ws/src/raspi_led_device_driver/myled.o /home/ubuntu/catkin_ws/src/raspi_led_device_driver/myled.mod.o
